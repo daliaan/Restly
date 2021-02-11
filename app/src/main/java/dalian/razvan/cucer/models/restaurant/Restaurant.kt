ef225cb5ff3a278d4b51ba503baf8f8af378e646
cucer razvan <razvan@kuwaitnet.com>
2021-02-11 20:39:59 +0200

    restaurant object created

---
+++ app/src/main/java/dalian/razvan/cucer/models/restaurant/Restaurant.kt
@@ -0,0 +1,43 @@
+package dalian.razvan.cucer.models.restaurant
+
+import android.os.Parcel
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+
+data class Restaurant(@SerializedName("categories") val categories: String = "",
+                      @SerializedName("rating") val rating: Double = 0.0,
+                      @SerializedName("id") val id: Int = 0,
+                      @SerializedName("title") val title: String = "",
+                      @SerializedName("imageUrl") val imageUrl: String = ""): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readDouble(),
+        parcel.readInt(),
+        parcel.readString().toString(),
+        parcel.readString().toString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(categories)
+        parcel.writeDouble(rating)
+        parcel.writeInt(id)
+        parcel.writeString(title)
+        parcel.writeString(imageUrl)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Restaurant> {
+        override fun createFromParcel(parcel: Parcel): Restaurant {
+            return Restaurant(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Restaurant?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
