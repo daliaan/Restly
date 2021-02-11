a5837b806a15fa97680c4f6d5c21c4308bda9ef7
cucer razvan <razvan@kuwaitnet.com>
2021-02-11 19:06:15 +0200

    splash fragment created; animations added; navigation graph updated

---
+++ app/src/main/java/dalian/razvan/cucer/screens/splash/SplashFragment.kt
@@ -0,0 +1,8 @@
+package dalian.razvan.cucer.screens.splash
+
+import dalian.razvan.cucer.R
+import dalian.razvan.cucer.core.baseClasses.BaseFragment
+
+class SplashFragment: BaseFragment() {
+    override fun whichLayout(): Int = R.layout.fragment_splash
+}
