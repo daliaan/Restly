9510451b295034196cdf673c243c14bd65d35bad
cucer razvan <razvan@kuwaitnet.com>
2021-02-14 12:47:23 +0200

    restaurant cell created

--- app/src/main/java/dalian/razvan/cucer/customViews/recyclerViews/RestaurantAdapter.kt
+++ app/src/main/java/dalian/razvan/cucer/customViews/recyclerViews/RestaurantAdapter.kt
@@ -1,5 +1,22 @@
 package dalian.razvan.cucer.customViews.recyclerViews
 
-class RestaurantAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import dalian.razvan.cucer.R
+import dalian.razvan.cucer.core.baseClasses.BaseModel
+import dalian.razvan.cucer.core.baseClasses.BaseRecyclerViewAdapter
+import dalian.razvan.cucer.models.restaurant.Restaurant
 
+class RestaurantAdapter: BaseRecyclerViewAdapter<Restaurant, RestaurantViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestaurantViewHolder
+        = RestaurantViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.restaurant_cell, parent, false))
+
+    override fun onBindViewHolder(holder: RestaurantViewHolder, position: Int) {
+        holder.bind(getItem(position), onClickListener)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun getItem(position: Int): Restaurant = items[position]
 }
---
+++ app/src/main/java/dalian/razvan/cucer/customViews/recyclerViews/RestaurantViewHolder.kt
@@ -0,0 +1,12 @@
+package dalian.razvan.cucer.customViews.recyclerViews
+
+import android.view.View
+import dalian.razvan.cucer.core.baseClasses.BaseRecyclerViewHolder
+import dalian.razvan.cucer.models.restaurant.Restaurant
+
+class RestaurantViewHolder(itemView: View): BaseRecyclerViewHolder<Restaurant>(itemView) {
+
+    override fun bind(item: Restaurant, onClickListener: View.OnClickListener) {
+        TODO("Not yet implemented")
+    }
+}
