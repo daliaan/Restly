d6a5c407e15a4790a4b02cedfa416ccc0c1ef44c
cucer razvan <razvan@kuwaitnet.com>
2021-02-14 21:32:39 +0200

    allergens list setup done

--- app/src/main/java/dalian/razvan/cucer/customViews/recyclerViews/allergens/AllergensViewHolder.kt
+++ app/src/main/java/dalian/razvan/cucer/customViews/recyclerViews/allergens/AllergensViewHolder.kt
@@ -4,10 +4,11 @@ import android.view.View
 import dalian.razvan.cucer.core.baseClasses.BaseRecyclerViewHolder
 import dalian.razvan.cucer.core.baseClasses.RecyclerViewItemClickListener
 import dalian.razvan.cucer.models.product.Allergen
+import kotlinx.android.synthetic.main.allergen_cell.view.*
 
 class AllergensViewHolder(itemView: View): BaseRecyclerViewHolder<Allergen>(itemView) {
 
     override fun bind(item: Allergen, onItemClick: RecyclerViewItemClickListener<Allergen>) {
-        TODO("Not yet implemented")
+        itemView.allergen.text = item.title
     }
 }
--- app/src/main/java/dalian/razvan/cucer/customViews/recyclerViews/product/ProductViewHolder.kt
+++ app/src/main/java/dalian/razvan/cucer/customViews/recyclerViews/product/ProductViewHolder.kt
@@ -1,9 +1,12 @@
 package dalian.razvan.cucer.customViews.recyclerViews.product
 
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
 import dalian.razvan.cucer.core.baseClasses.BaseRecyclerViewHolder
 import dalian.razvan.cucer.core.baseClasses.RecyclerViewItemClickListener
+import dalian.razvan.cucer.customViews.recyclerViews.allergens.AllergensAdapter
+import dalian.razvan.cucer.models.product.Allergen
 import dalian.razvan.cucer.models.product.Product
 import kotlinx.android.synthetic.main.product_cell.view.*
 import kotlinx.android.synthetic.main.restaurant_cell.view.*
@@ -20,5 +23,15 @@ class ProductViewHolder(itemView: View): BaseRecyclerViewHolder<Product>(itemVie
         }
         itemView.product_price.text = item.price.toString()
 
+        itemView.allergen_list.layoutManager = LinearLayoutManager(itemView.context, LinearLayoutManager.HORIZONTAL, false)
+        itemView.allergen_list.setHasFixedSize(false)
+        val adapter = AllergensAdapter()
+        adapter.addItemClickListener(object: RecyclerViewItemClickListener<Allergen>{
+            override fun onItemClick(item: Allergen) {
+
+            }
+        })
+        itemView.allergen_list.adapter = adapter
+        adapter.resetList(adapter.convertStringsArray(item.allergens))
     }
 }
